160cd752629657f9173e71a10c81138eeb66980f
Bence Király <kiralybencepecs@gmail.com>
2021-05-30 22:55:37 +0200

    Added TODO

--- commands/XwordCommand.js
+++ commands/XwordCommand.js
@@ -8,6 +8,7 @@ module.exports = class extends Command {
     }
 
     shouldRun(msg) {
+        // TODO: this should contain the permissionMatches and hasEnoughParams checkings (without duplicating code)
         return this.getValidLetter(msg)
     }
 
