056f55eee5f7d45ad346271f0723466868693880
Bence Király <kiralybencepecs@gmail.com>
2024-09-20 21:23:04 +0200

    Fixed indentation

--- classes/Sergey.js
+++ classes/Sergey.js
@@ -58,7 +58,7 @@ module.exports = class Sergey {
                 Discord.GatewayIntentBits.MessageContent,
                 Discord.GatewayIntentBits.GuildMessageReactions,
                 Discord.GatewayIntentBits.GuildEmojisAndStickers,
-              ],
+            ],
         });
 
         Sergey.client.on('ready', () => {
