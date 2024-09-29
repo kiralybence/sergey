eb69960182caf78ebb9c2a213e2e89356b41579d
Bence Király <kiralybencepecs@gmail.com>
2024-09-29 21:30:48 +0200

    Added limit 1 to emote query

--- classes/Emote.js
+++ classes/Emote.js
@@ -2,6 +2,6 @@ const DB = require('./DB');
 
 module.exports = class Emote {
     static async get(name) {
-        return (await DB.query('select * from emotes where name = ?', [name]))?.[0]?.tag;
+        return (await DB.query('select * from emotes where name = ? limit 1', [name]))?.[0]?.tag;
     }
 };
