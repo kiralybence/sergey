175b151db863b04693f3b5d48a3666e14a95baea
Bence Király <kiralybencepecs@gmail.com>
2024-10-04 09:03:29 +0200

    DM ChannelType check

--- middlewares/LogToConsole.js
+++ middlewares/LogToConsole.js
@@ -1,10 +1,11 @@
 const Middleware = require('./Middleware');
 const Log = require('../classes/Log');
 const Formatter = require('../classes/Formatter');
+const Discord = require('discord.js');
 
 module.exports = class LogToConsole extends Middleware {
     async run(message) {
-        let breadcrumbs = message.channel.type === 'dm'
+        let breadcrumbs = message.channel.type === Discord.ChannelType.DM
             ? [
                 'DM',
                 (message.author.globalName || message.author.username),
